3fdaa71d8eb0170babec04f72a022bcdb7526142
Laura Delaney <lauradelaney115@gmail.com>
2022-09-28 12:43:20 -0700

    Added text, and started first while loop

--- start.py
+++ start.py
@@ -1,8 +1,24 @@
 def main():
-    bedroom()
+    bathroom()
+
+head_pounding = True
+
+def bathroom():
+    directions = ['front','left','right']
+    print("""You wake up in a cold semi spherical contraption. A tub. That's what it's called.
+    The mostly smooth white veneer of, well, not porceline.
+    Your head is pounding as you clamber your way out of the tub with the grace of an ostrich.
+    There is a door directly in FRONT of you, a medicine cabinet on your LEFT, and a nearly identical door on your RIGHT.""")
+
+    userInput = ''
+    while userInput not in directions:
+        userInput = input('> ')
+    
+    
 
 
 
+main()
 
 if __name__ == "__main__":
-    main()
+    main
