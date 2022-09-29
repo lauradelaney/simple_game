d83c0e37b1f2f08d5defe9ee8e691cb99eadfec0
Laura Delaney <lauradelaney115@gmail.com>
2022-09-29 09:59:23 -0700

    Small changes before pushing

--- start.py
+++ start.py
@@ -6,26 +6,26 @@ overdose = False
 inventory = []
 
 def intro():
-    directions = ['front','left','right']
+    directions = ['south','east','west']
     print("""You wake up in a cold semi spherical contraption. A tub. That's what it's called.
 The mostly smooth white veneer of, well, not porceline.
 Your head is pounding as you clamber your way out of the tub with the grace of an ostrich.
-There is a door directly in FRONT of you, a medicine cabinet on your LEFT, and a nearly identical door on your RIGHT.""")
+There is a door directly in SOUTH of you, a medicine cabinet to your EAST, and a nearly identical door on your WEST.""")
 
     pill_count  = 0
     userInput = ''
-    while userInput not in directions or userInput == 'left':
+    while userInput not in directions or userInput == 'east':
         userInput = input('> ').lower()
-        if userInput == 'front':
+        if userInput == 'south':
             print("You approach the dark brown door, and grasp the brassing handle.")
             bedroom()
         
-        elif userInput == 'right':
+        elif userInput == 'west':
             print("You approach the dark brown door, and grasp the brassing handle.")
             livingroom()
 
         #need to add pills to inventory, and set up inventory system
-        elif userInput == 'left':
+        elif userInput == 'east':
             global head_pounding
             head_pounding = False
             print("You see a grizzy figure in the mirror. Your own reflection. It took you a minute to recognize yourself...")
@@ -65,23 +65,31 @@ def bathroom():
 
 
 def bedroom():
-    directions = ['dig','left','behind']
+    directions = ['dig','west','north']
     print("""
 You enter a space that feels like it has seen a thousand lifetimes.
 None of those lifetimes have been cleaners, though...
 But through the mess you can tell, at one point, this was a nice, enjoyable space.
 You can DIG through your own filth, possibly find something useful. Now that you think about it, your vision is blurrier than you remember.
-You can also see a door to your LEFT, and you can re-enter the bathroom BEHIND you.
+You can also see a door to your WEST, and you can re-enter the bathroom NORTH of you.
 """)
     userInput = ''
-    while userInput not in directions:
+    while userInput not in directions or userInput == 'dig':
         userInput = input('> ').lower()
-        if userInput == 'behind':
-            pass
+        if userInput == 'north':
+            bathroom()
 
-        if userInput == 'dig':
+        elif userInput == 'dig':
             print("You search the room. Not finding much besides your ex's old hoodie, a fork that you do not recognize, and a pair of glasses.")
             print("You put on the glasses. It is uncertain if these are yours. Your vision remains blurry, but in a different way.")
+            global inventory
+            if 'glasses' not in inventory:
+                inventory.append('glasses')
+                continue
+            continue
+
+        elif userInput == 'west':
+            livingroom()
 
 
 main()
