d90e1532a62bde19efbcf6e244807b8914c03432
Laura Delaney <lauradelaney115@gmail.com>
2022-10-03 11:27:01 -0700

    updated logic for inventory

--- start.py
+++ start.py
@@ -5,15 +5,27 @@ def main():
 rooms = {
     'bathroom': {
         'name': 'bathroom',
-        'desc': 'a room that smells truly damp',
-        'north': 'hallway',
+        'desc': 'a room that smells truly damp. There is a slightly ajar door to the SOUTH.',
+        'south': 'bedroom',
+        'search': 'Glasses',
+        'search_desc': 'You find a pair of glasses scattered across the floor.'        
+    },
+
+    'bedroom': {
+        'name': 'bedroom',
+        'desc': '',
+        'north': 'bathroom',
+        'west': 'hallway'
     }
+
 }
 
 current_room = rooms['bathroom']
-directions = ['north','south','east','west']
+directions = ['north','south','east','west','search']
+inventory = []
 
 def game_loop():
+    #want to fix this so only the input gets repeated and not this whole section.
     while True:
         global current_room
         print(
@@ -23,18 +35,31 @@ def game_loop():
             f"This is the {current_room['name']}"
         )
         
+
+        #need to refactor this so the logic isn't so messy.
         userInput =  ''
         while userInput not in directions:
             userInput = input('> ').lower().strip()
             if userInput in directions:
-                if userInput in current_room:
+                if userInput in current_room and userInput != 'search':
                     current_room = rooms[current_room[userInput]]
-                    print(current_room)
-                    break
-                else:
+
+                elif userInput == 'search' and 'search' not in current_room:
+                    print("Nothing to search for.")
                     continue
 
-game_loop()
+                elif userInput == 'search' and 'search' in current_room:
+                    global inventory
+                    if current_room['search'] not in inventory:
+                        inventory.append(current_room['search'])
+                        print(f"You dig around and find {current_room['search_desc']}")
 
-#if __name__ == "__main__":
-#   main()
+
+                else:
+                    print("Can't go that way.")
+                    ...
+                    
+
+
+if __name__ == "__main__":
+   main()
